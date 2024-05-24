983c00107b35afddf1d79ddbc0f35936750a0850
Andrey <ykt_andrey@mail.ru>
2024-05-25 00:30:30 +0300

    WebUI:init angular application; create base layout

---
+++ WebUI/src/app/shared/components/navigation/navigation.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+import { RouterLink, RouterLinkActive } from "@angular/router";
+
+@Component({
+  selector: "app-navigation",
+  standalone: true,
+  templateUrl: "./navigation.component.html",
+  styleUrl: "./navigation.component.css",
+  imports: [RouterLink, RouterLinkActive],
+})
+export class NavigationComponent {}
